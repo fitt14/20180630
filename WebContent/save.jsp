41605ecfff7e3457240a965da78a18769b2c5c16
Administrator <Administrator@JN4G30Z8QM8G6CR>
2018-07-04 15:44:22 +0800

    1: 把两个jar包添加lib文件夹中 2: 完成query.jsp页面显示数据

--- WebContent/save.jsp
+++ WebContent/save.jsp
@@ -3,12 +3,28 @@
 <html>
 <head>
 <title>Insert title here</title>
+  
 </head>
 <body>
+    <!-- java代码声明,在Jsp有3大内置对象用来存储数据: request  session application 
+        
+           1: 存储到application对象中的数据与项目的生命周期相同,如果项目重启则app数据将会消失
+                application可以存储系统公共的数据(类似内存的一个缓存区域)
+           2： session(会话,每个用户都有独立的区域,但是只要会话结束则session消失)
+                                会话结束两种情况: 默认情况下30分钟没有与服务器交互, 2: 浏览器关闭
+           3: request: 代表的是用户与服务器端的一次请求.每次请求都会产生一个request对象
+                               如果当前用户产生一个新request对象,则原来的request将会自动销毁
+    -->
+    <% 
+        request.setAttribute("req", "request");
+    	session.setAttribute("ses", "session");
+    	application.setAttribute("app", "application");
+    %>
+    <a href="query.jsp">query.jsp</a>
+
    <!-- 发送的是http请求,ProductService是不能够识别,需要一个中介Servlet -->
    <!-- 在项目中所有的请求都要从工程名开始,请修改成自己工程名 -->
-   <!-- get post -->
-   <form action="/java0630/ProductServlet" method="post">
+   <form action="/20180630/ProductServlet" method="post">
       <!--  ctrl + atl + 下方向键 -->
            商品名:<input type="text" name="name" /><br/>
             价格:<input type="text" name="price" /><br/>
--- src/cn/yd/oa/servlet/ProductServlet.java
+++ src/cn/yd/oa/servlet/ProductServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import cn.yd.oa.model.Product;
 import cn.yd.oa.service.ProductService;
@@ -25,9 +26,14 @@ public class ProductServlet extends HttpServlet {
 		String keyword = request.getParameter("keyword");
 		// 2: 调用业务逻辑
 		ArrayList<Product> proList = productService.queryByName(keyword);
-		for(Product temp: proList) {
-			System.out.println(temp);
-		}
+		System.out.println(proList.size());
+		// 此数据要交给query.jsp页面显示
+		request.setAttribute("proList", proList);
+		HttpSession session = request.getSession();
+		session.setAttribute("proList",proList);
+		// 当前页面跳转到query.jsp
+		response.sendRedirect("/20180630/query.jsp");
+		// json
 	}
 	// 处理:method="post"
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -42,7 +48,7 @@ public class ProductServlet extends HttpServlet {
 		// 2: 调用业务逻辑
 		productService.save(product);
 		// 3: 跳转到查询页面,在Java开发中所有的资源访问都要从工程名开始(query.jsp)
-		response.sendRedirect("/demo/query.jsp");
+		response.sendRedirect("/20180630/query.jsp");
 	}
 
 }
